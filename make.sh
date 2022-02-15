fee4b6ba43984889fa9bf186c9f4746c4e4c66a9
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-14 19:38:28 -0800

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Patching files..."
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040147351359578\/emote.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/933578895574302760\/spunch.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040122609147966\/calcponder_nobg.png/g' {} \;
+sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
+echo "Patched files!"
+yarn
+NODE_ENV=production npm run build
+NODE_ENV=production npm run migrate
+echo "Done!"
