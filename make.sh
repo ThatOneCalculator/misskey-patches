e14f591ca20bd995dab2993811060a1d58f128e0
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-16 17:42:25 -0800

    Change theme color

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/cdn
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/933578895574302760\/spunch.png/g' {} \;
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040122609147966\/calcponder_nobg.png/g' {} \;
 sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
+sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
 echo "Patched files!"
 yarn
 NODE_ENV=production npm run build
