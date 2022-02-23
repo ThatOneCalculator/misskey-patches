4b66933ab5f1a6fcaa4f2795bef109bb2a141605
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-23 15:43:23 -0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -6,10 +6,10 @@ fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/cdn.
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040122609147966\/calcponder_nobg.png/g' {} \;
 sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
 sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
+sed -i '20s/false/true/' ./packages/client/src/store.ts
+sed -i "56s/'🎉', '💢', '😥', '😇'/'😥', '😇', ':calchigh:', ':calcmentalbreakdowncrying:', ':amongass:'/" ./packages/client/src/store.ts
 wget -q https://github.com/ThatOneCalculator/misskey-patches/raw/main/favicon.ico
-wget -q https://github.com/ThatOneCalculator/misskey-patches/raw/main/store.ts
 mv ./favicon.ico ./packages/backend/assets/
-mv ./store.ts ./packages/client/src/
 echo "Patched files!"
 yarn
 NODE_ENV=production npm run build
