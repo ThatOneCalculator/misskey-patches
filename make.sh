30df1a0f968c84a7e5f356dd19e225ff9502b004
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-25 14:23:06 -0800

    Favicon, gruvbox

--- make.sh
+++ make.sh
@@ -11,10 +11,12 @@ sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/cli
 sed -i 's/Misskey expands the world of the Fediverse/Stop posting about Among Us/g' ./locales/en-US.yml
 sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
 cp patches/favicon.ico ./packages/backend/assets/favicon.ico
+cp patches/favicon.png ./packages/backend/assets/favicon.png
 cp patches/apple-touch-icon.png ./packages/backend/assets/apple-touch-icon.png
 cp patches/user-unknown.png ./packages/backend/assets/icons/user-unknown.png
 cp patches/192.png ./packages/backend/assets/icons/192.png
 cp patches/512.png ./packages/backend/assets/icons/512.png
+cp patches/*.json5 ./packages/client/src/themes/
 git apply patches/store.patch &> /dev/null
 echo "Patched files!"
 yarn
