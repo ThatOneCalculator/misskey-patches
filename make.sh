8d959a75c1eddbfea7600dee8bb3ae1498e9454b
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-24 23:44:55 -0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Patching files..."
+git clone --quiet https://github.com/rose-pine/misskey.git rosepinethemes &> /dev/null
+mv ./rosepinethemes/*.json5 ./packages/client/src/themes/
+rm -rf ./rosepinethemes
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040147351359578\/emote.png/g' {} \;
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/933578895574302760\/spunch.png/g' {} \;
 fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040122609147966\/calcponder_nobg.png/g' {} \;
