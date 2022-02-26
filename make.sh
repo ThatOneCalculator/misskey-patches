665127e702f26e3c74c9c20e13ca9bf940e5cb07
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-25 16:15:55 -0800

    GHBASE

--- make.sh
+++ make.sh
@@ -4,9 +4,10 @@ echo "Patching files..."
 git clone --quiet https://github.com/rose-pine/misskey.git rosepinethemes &> /dev/null
 mv ./rosepinethemes/*.json5 ./packages/client/src/themes/
 rm -rf ./rosepinethemes
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/error.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/info.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/not-found.png/g' {} \;
+GHBASE='https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/'
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/${GHBASE}error.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/${GHBASE}info.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/${GHBASE}not-found.png/g' {} \;
 sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
 sed -i 's/Misskey expands the world of the Fediverse/Stop posting about Among Us/g' ./locales/en-US.yml
 sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
