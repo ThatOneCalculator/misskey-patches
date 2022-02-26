d5fd7705c7912a6e4423d21d48607df3e76a1276
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-25 16:13:20 -0800

    Remove discord cdn links

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@ echo "Patching files..."
 git clone --quiet https://github.com/rose-pine/misskey.git rosepinethemes &> /dev/null
 mv ./rosepinethemes/*.json5 ./packages/client/src/themes/
 rm -rf ./rosepinethemes
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040147351359578\/emote.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/933578895574302760\/spunch.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/cdn.discordapp.com\/attachments\/810799100940255260\/941040122609147966\/calcponder_nobg.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/error.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/info.png/g' {} \;
+fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/not-found.png/g' {} \;
 sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
 sed -i 's/Misskey expands the world of the Fediverse/Stop posting about Among Us/g' ./locales/en-US.yml
 sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
