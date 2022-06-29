ed1a6254edd3f14eff0856124bb53ce251cb1fb0
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-06-28 19:47:21 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@ echo "Patching files..."
 git clone --quiet https://github.com/rose-pine/misskey.git rosepinethemes &> /dev/null
 mv ./rosepinethemes/*.json5 ./packages/client/src/themes/
 rm -rf ./rosepinethemes
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/error.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/error.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/info.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/info.png/g' {} \;
-fdfind -t f -x sed -i 's/https:\/\/xn--931a.moe\/assets\/not-found.jpg/https:\/\/raw.githubusercontent.com\/ThatOneCalculator\/misskey-patches\/main\/not-found.png/g' {} \;
+fdfind -t f -x sed -i 's/badges\/info.jpg/badges\/info.png/g' {} \;
+fdfind -t f -x sed -i 's/badges\/not-found.jpg/badges\/not-found.png/g' {} \;
+fdfind -t f -x sed -i 's/badges\/error.jpg/badges\/error.png/g' {} \;
 sed -i 's/https:\/\/www.google.com/https:\/\/gowogle.voring.me/g' ./packages/client/src/components/google.vue
 sed -i 's/Misskey expands the world of the Fediverse/Stop posting about Among Us/g' ./locales/en-US.yml
 sed -i 's/86b300/00beff/g' ./packages/backend/src/server/web/views/base.pug
@@ -18,6 +18,9 @@ cp patches/apple-touch-icon.png ./packages/backend/assets/apple-touch-icon.png
 cp patches/user-unknown.png ./packages/backend/assets/user-unknown.png
 cp patches/192.png ./packages/backend/assets/icons/192.png
 cp patches/512.png ./packages/backend/assets/icons/512.png
+cp patches/info.png ./packages/backend/assets/icons/badges/info.png
+cp patches/not-found.png ./packages/backend/assets/icons/badges/not-found.png
+cp patches/error.png ./packages/backend/assets/icons/badges/error.png
 cp patches/512.png ./packages/client/assets/about-icon.png
 cp patches/fedi.jpg ./packages/client/assets/fedi.jpg
 cp patches/*.json5 ./packages/client/src/themes/
